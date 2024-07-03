0353d21ff181772a6e9feeb6db44919c24d76452
Indra Lukmana <indralukmana@live.com>
2024-07-03 10:16:49 +0700

    fix: :bug: fix hydration warnings on html and alchemy fetch ssr

--- src/lib/alchemy.ts
+++ src/lib/alchemy.ts
@@ -1,11 +1,20 @@
 import { Alchemy, AlchemySettings, Network } from "alchemy-sdk";
+import { headers } from "next/headers";
+
+const baseUrl = `${headers().get("x-forwarded-proto")}://${headers().get(
+  "host"
+)}`;
 
 const alchemySettings = {
   apiKey: process.env.ALCHEMY_API_KEY,
   network: Network.ETH_SEPOLIA,
   connectionInfoOverrides: {
-    // need to do this to properly setup ethers.js in serverside https://github.com/alchemyplatform/alchemy-sdk-js/issues/400#issuecomment-2151351091
-    skipFetchSetup: true,
+    // need to do this to properly setup ethers.js in serverside
+    // https://github.com/alchemyplatform/alchemy-sdk-js/issues/400#issuecomment-2151351091
+    // https://github.com/ethers-io/ethers.js/issues/4469#issuecomment-1932145334
+    fetchOptions: {
+      referrer: baseUrl,
+    },
   },
 } satisfies AlchemySettings;
 
